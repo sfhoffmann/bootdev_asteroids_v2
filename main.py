c63a1a35ba6106d2f06142ce9fb91ddba03b2209
sfhoffmann <sean.f.hoffmann@gmail.com>
2026-03-13 08:56:10 -0700

    initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH
+
+def main():
+    print(f"Starting Asteroids with pygame version {pygame.version.ver}")
+
+
+if __name__ == "__main__":
+    main()
